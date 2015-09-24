63bd906526f8f1dba2a72f5f8d4f2992bc363a9f
Vladislav Radchenko <v.radchenko@madnet-team.ru>
2015-09-24 12:43:15 +0300

    progressbar examples

--- src/main/java/MainController.java
+++ src/main/java/MainController.java
@@ -3,9 +3,17 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
+import json.MatchDetails;
+import json.MatchHistory;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainController {
-
+    private static final int MATCHES_AMOUNT = 20;
+    private ApiController api = new ApiController();
+    private KdaCalculator calc = new KdaCalculator();
     @FXML
     private TextField steamIdTextArea;
 
@@ -32,6 +40,30 @@ public class MainController {
     }
 
     public void calculate() {
-        resultText.setText("asdasd");
+        String s1 = steamIdTextArea.getText();
+        int accountId = Integer.parseInt(s1);
+        MatchHistory history = null;
+        try {
+            try {
+                history = api.getMatchHistory(MATCHES_AMOUNT, accountId);
+            } catch (IOException e) {
+                resultText.setText(e.getMessage());
+            }
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Task can't be completed");
+            throw new RuntimeException("Closing ...");
+        }
+        List<Long> matchesList = calc.matchIdList(history);
+        ArrayList<MatchDetails> matchDetailses = null;
+        try {
+            matchDetailses = calc.mathDetailsArray(matchesList);
+        } catch (IOException e) {
+            resultText.setText(e.getMessage());
+        }
+        List<int[]> scoreList = calc.playerScore(matchDetailses, accountId);
+        double averageKDA = calc.averageKda(scoreList);
+        String s = "Average KDA for last " + MATCHES_AMOUNT + " rating games = " + averageKDA;
+        resultText.setText(s);
     }
 }
